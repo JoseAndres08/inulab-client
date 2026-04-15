0bd45ae7800862d012f6b5c8a62ff8d552b44d8e
JoseAndres08 <josenadrescovenas@gmail.com>
2026-04-15 10:00:22 -0500

    inulab-client: feat(search): resuelto el buscador

--- inulab-client/js/app.js
+++ inulab-client/js/app.js
@@ -6397,7 +6397,7 @@ const PdfViewer = ({ url, style, className }) => {
                                                 {/* Header fijo */}
                                                 <div className="flex-shrink-0 sticky top-0 z-20 bg-gray-100 px-6 py-2">
 
-                                                    <div className="flex items-center gap-4 border border-gray-200 rounded-xl px-4 py-2 bg-gray-100">
+                                                    <div className="flex items-center gap-4">
 
                                                         {/* Título */}
                                                         <div className="flex items-center gap-2 min-w-max">
