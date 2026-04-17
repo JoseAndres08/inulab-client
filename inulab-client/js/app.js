e7f449673a0cb51ce6468f37b9360f94b4e364d2
JoseAndres08 <josenadrescovenas@gmail.com>
2026-04-17 09:22:51 -0500

    resuelto25

--- inulab-client/js/app.js
+++ inulab-client/js/app.js
@@ -6630,7 +6630,7 @@ const PdfViewer = ({ url, style, className }) => {
                                             <div className="flex flex-col flex-1 h-full -mx-4 lg:-mx-6 bg-gray-100 overflow-hidden">
 
                                                 {/* Header fijo */}
-                                                <div className="flex-shrink-0 sticky top-0 z-50 bg-gray-100 px-6 pt-1 pb-3 border-b border-gray-200">
+                                                <div className="flex-shrink-0 sticky top-0 z-50 bg-white px-6 pt-3 pb-3 border-b border-gray-200 shadow-sm">
 
                                                     <div className="flex items-center gap-4">
 
